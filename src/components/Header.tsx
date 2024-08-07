49b4d73405abba3300c345045b4ac9194fe216a7
Alister Huysmans <alister.huysmans@gmail.com>
2024-08-07 02:09:08 +0200

    Improved Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,24 @@
-import { SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { ThemeToggler } from "./ThemeToggler";
+import { CloudUpload } from "lucide-react";
+import { Button } from "./ui/button";
 
 function Header() {
     return (
         <header className="relative flex justify-between items-center px-5 py-3 gap-2 z-20">
-            <Link href={"/"} className="text-lg flex-1 font-semibold">
-                Cloud Drive
-            </Link>
+            <div className="flex-1">
+                <Link href={"/"} className="w-fit text-lg font-semibold">
+                    Cloud Drive
+                </Link>
+            </div>
+            <SignedIn>
+                <Button variant="link" className="px-0">
+                    <Link href={"/dashboard"}>
+                        <CloudUpload strokeWidth={1.5} />
+                    </Link>
+                </Button>
+            </SignedIn>
             <ThemeToggler />
             <UserButton />
             <SignedOut>
